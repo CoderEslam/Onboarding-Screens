9791633e4c93b5eed3f16d45f4de5c7875f315ae
CoderEslam <eslamghazy600@gmail.com>
2023-09-04 19:46:58 +0200

    Initial Commit

---
+++ Onboarding Screens/Onboarding_ScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_ScreensApp.swift
+//  Onboarding Screens
+//
+//  Created by Eslam Ghazy on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_ScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
